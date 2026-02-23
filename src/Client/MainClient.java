27f7db5320f156a6870f16152a920a298c76dd0c
clementig201 <clementig201@gmail.com>
2026-02-23 11:08:21 +0100

    1

--- src/Client/MainClient.java
+++ src/Client/MainClient.java
@@ -1,15 +1,27 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+package Client;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class MainClient {
+    public static void main(String[] args) {
+
+         System.out.println("Client: avvio del client");
+        try {
+            // 3 istanzio il socket dell client
+            Socket socket = new Socket("localhost",3000);
+            // 4 il client srive un messaggio
+            System.out.println("Client: il client si è connesso al server");
+            OutputStream outputStream = socket.getOutputStream();
+            PrintWriter pw = new PrintWriter(outputStream);
+            //  5 stiamo scrivendo nella ram del client nell output stream
+            pw.print("Ciao server\n");
+            //  5 abbiamo spedito il messaggio al server liberando un area di memoria
+            pw.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
-}
+    }
--- src/Server/MainServer.java
+++ src/Server/MainServer.java
@@ -1,4 +1,29 @@
-package Client;
+package Server;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class MainServer {
+    public static void main(String[] args) {
+
+        System.out.println("Server: inizio esecuzione");
+
+        try {
+            // 1 istanzio il socket del server
+            ServerSocket server = new ServerSocket(3000);
+            System.out.println("Server: in attesa di client");
+            // 2 invoco il metodo accept
+            Socket clientSocket = server.accept();
+            System.out.println("Server: il client si è connesso");
+
+            // 6 ilserver deve leggere ciò che gli è stato inviato
+            InputStream inputStream = clientSocket.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+            String messaggio = br.readLine();
+            System.out.println("il client"+ clientSocket + "ha scritto" + messaggio);
+        } catch (IOException e) {
+            System.err.println("porta non disponibile," +
+                    "errore nella creazione della connection socket");
+        }
+    }
 }
